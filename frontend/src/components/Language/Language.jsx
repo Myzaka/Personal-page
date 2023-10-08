ac1cb9e308076c21387a48d220fdd462b3d04d9e
Jerome <jerome.semik@bbox.fr>
2023-10-08 19:17:16 +0200

    Mise en forme boutons sélection langue

--- frontend/src/components/Language/Language.jsx
+++ frontend/src/components/Language/Language.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLang } from '../Generic/Language_context';
-import './Language_style.scss'
+import './Language_style.scss';
 
 export default function LanguageSelector() {
-
   const { changeLanguage } = useLang();
+  const [selectedButton, setSelectedButton] = useState('fr');
+
+  const handleLanguageChange = (newLanguage) => {
+    changeLanguage(newLanguage);
+    setSelectedButton(newLanguage);
+  };
 
   return (
-    <div>
-      <button onClick={() => changeLanguage('fr')}>FR</button>
-      <button onClick={() => changeLanguage('en')}>EN</button>
+    <div className="languageSelector">
+      <button
+        className={`languageSelector__button ${
+          selectedButton === 'fr' ? 'languageSelector__button--selected' : ''
+        }`}
+        onClick={() => handleLanguageChange('fr')}
+      >
+        FR
+      </button>
+      <button
+        className={`languageSelector__button ${
+          selectedButton === 'en' ? 'languageSelector__button--selected' : ''
+        }`}
+        onClick={() => handleLanguageChange('en')}
+      >
+        EN
+      </button>
     </div>
   );
 }
