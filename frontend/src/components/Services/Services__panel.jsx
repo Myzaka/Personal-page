c5a85eb4222bd656ae118895b443eda45b16c8db
Jerome <jerome.semik@bbox.fr>
2023-10-10 21:45:37 +0200

    Format header et Services

--- frontend/src/components/Services/Services__panel.jsx
+++ frontend/src/components/Services/Services__panel.jsx
@@ -1,26 +1,3 @@
-/*import React from 'react';
-import './Services_style.scss';
-import texts from './Services_text.json';
-import { useLang } from '../Generic/Language_context';
-
-export default function ServicesPanel({ title, sentence }) {
-  const { language } = useLang();
-  const text = texts[language];
-  const sentences = sentence.split('.');
-  
-  // Supprimer les phrases vides (contenant uniquement un point)
-  const filteredSentences = sentences.filter(e => e.trim() !== '');
-
-  return (
-    <div className='services__panel'>
-      <h3 className='services__panel--title'>{text[title]}</h3>
-      {filteredSentences.map((e, index) => (
-        <p className='services__panel--text' key={index}>{e.trim()}.</p>
-      ))}
-    </div>
-  );
-}*/
-
 import React, { useState } from 'react';
 import './Services_style.scss';
 import texts from './Services_text.json';
@@ -46,22 +23,26 @@ export default function ServicesPanel({ title, sentence }) {
     <div className='services__panel'>
       <h3
         className={`services__panel--title ${isHovered ? 'services__panel--titleColored' : 'services__panel--title'}`}
-        //className='services__panel--title'
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
         {text[title]}
       </h3>
-      {filteredSentences.map((e, index) => (
-        <p
-          key={index}
-          className={`${
-            isHovered ? 'services__panel--textVisible' : 'services__panel--textHidden'
-          }`}
-        >
-          {e.trim()}.
-        </p>
-      ))}
+        <div className={`${
+                isHovered ? 'services__panel--textVisible' : 'services__panel--textHidden'
+              }`}>
+          {filteredSentences.map((e, index) => (
+            <p
+              key={index}
+              /*className={`${
+                isHovered ? 'services__panel--textVisible' : 'services__panel--textHidden'
+              }`}*/
+            >
+              {e.trim()}.
+            </p>
+          ))}
+        </div>
+      
     </div>
   );
 }
