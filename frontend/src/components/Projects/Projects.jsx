c78bbfda1268975f174db504e9e07e6e59d79c73
Jerome <jerome.semik@bbox.fr>
2023-10-31 20:52:18 +0100

    Sauvegardes images projets

--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -4,8 +4,10 @@ import texts from './Projects_text.json';
 import textsProjects from './ProjectsList/ProjectisList_text.json'
 import { useLang } from '../Generic/Language_context';
 import CardPresentation from '../Generic/Card/Card';
+import projet2 from './ProjectsList/Risk.png';
 import projet4 from './ProjectsList/alert.png';
 import projet8 from './ProjectsList/rgpd.png';
+import projet9 from './ProjectsList/grimoire.png';
 import projet10 from './ProjectsList/KASA.png';
 
 export default function Projects() {
@@ -23,7 +25,7 @@ export default function Projects() {
                     />
                     <CardPresentation 
                         project="project2"
-                        image={projet10}
+                        image={projet2}
                     />
                     <CardPresentation 
                         project="project3"
@@ -51,7 +53,7 @@ export default function Projects() {
                     />
                     <CardPresentation 
                         project="project9"
-                        image={projet10}
+                        image={projet9}
                     />
                     <CardPresentation 
                         project="project10"
