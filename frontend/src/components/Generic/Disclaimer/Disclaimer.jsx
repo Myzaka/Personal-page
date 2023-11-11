db3a733a6fc241632d62d7d9791d97e71ba7cd5b
Jerome <jerome.semik@bbox.fr>
2023-11-11 17:48:54 +0100

    Bouton fermeture message d'alerte

--- frontend/src/components/Generic/Disclaimer/Disclaimer.jsx
+++ frontend/src/components/Generic/Disclaimer/Disclaimer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Disclaimer_style.scss';
 import texts from './Disclaimer_text.json';
 import { useLang } from '../../Generic/Language_context';
@@ -7,7 +7,18 @@ export default function Disclaimer() {
   const { language } = useLang();
   const text = texts[language];
 
-  return(
-    <h3 className='disclaimer'>{text['title']}</h3>
-  )
-  }
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  return isVisible ? (
+    <div className='disclaimer'>
+      <h3 className='disclaimer__text'>{text['title']}</h3>
+      <button className='closeButton' onClick={handleClose}>
+        X
+      </button>
+    </div>
+  ) : null;
+}
